5e2d7f5800adf04422eae56e41e7c3187d0bb252
Andrew Lalis <andrewlalisofficial@gmail.com>
2017-04-23 16:03:34 +0200

    Added icons to the main window and buttons.

--- src/net/agspace/Main.java
+++ src/net/agspace/Main.java
@@ -13,9 +13,11 @@ import java.awt.*;
 public class Main {
 
     public static final String TITLE = "Tengwar Typewriter";
+    public static final String ICON_PATH = "resources/icon.png";
 
     public static void main(String[] args){
         JFrame f = new JFrame(TITLE);
+        f.setIconImage(Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemResource(ICON_PATH)));
         f.setContentPane(new Window().getMainPanel());
         f.pack();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
